186c09d8d526485b9ee0bcecf066f6de1762c583
Kibreab Aklilu <Akkeba37@gmail.com>
2023-10-17 19:55:20 +0300

    Connecting to MongoDb

--- server-side/index.js
+++ server-side/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
-
+import connectDB from "./db/db.js";
 const app = express();
 
+connectDB();
+
 app.use(cors());
+app.use(express.json());
 
 const PORT = 3000;
 
